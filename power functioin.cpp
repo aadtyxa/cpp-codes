a5236bde9f4624072eb9a06bdd11bdcc6b4b7016
aadtyxa <aadityarawat948@gmail.com>
2024-08-14 18:35:25 +0530

    Create power functioin.cpp

---
+++ power functioin.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int power( int a , int b){
+    int ans = 1;
+    for ( int i = 1; i <= b; i++){
+        ans = ans * a;
+    }
+    return ans;
+}
+int main() {
+    int a,b;
+    cout<< " Enter the number  : "<<endl;
+    cin>> a ;
+    cout<< " Enter its power : "<<endl;
+    cin>> b;
+    int answer = power(a,b);
+    cout<<"power is "<<answer;
+}
